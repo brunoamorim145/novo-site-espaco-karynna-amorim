afa88d2280a0a8ef62729e695a9975ece35c06d6
brunoamorim145 <brunoamorim145@gmail.com>
2024-03-17 22:19:06 -0300

    Projeto Criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ServicosComponent } from './pages/servicos/servicos.component';
+import { LocalizacaoComponent } from './pages/localizacao/localizacao.component';
+import { ContatosComponent } from './pages/contatos/contatos.component';
+import { SobreNosComponent } from './pages/sobre-nos/sobre-nos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicosComponent,
+    LocalizacaoComponent,
+    ContatosComponent,
+    SobreNosComponent
   ],
   imports: [
     BrowserModule,
